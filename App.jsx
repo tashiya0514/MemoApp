c437721d536ea1235f3909da8f22c7dbea405c74
tashiya0514 <112812058+tashiya0514@users.noreply.github.com>
2022-09-06 21:45:53 +0900

    Create Edit Screen

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    // <MemoDetailScreen />
     // <MemoListScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }

1f0a0667730907aa8d3189c7898b3b88495e1213
tashiya0514 <112812058+tashiya0514@users.noreply.github.com>
2022-09-06 22:53:01 +0900

    Create Login and SignUp Screen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput value='Email Address' style={styles.input}></TextInput>
+        <TextInput value='Password' style={styles.input}></TextInput>
+        <Button label='hoge' />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already registered?</Text>
+          <Text style={styles.footerLink}>Log in.</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    backgroundColor: '#fff',
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  },
+})
